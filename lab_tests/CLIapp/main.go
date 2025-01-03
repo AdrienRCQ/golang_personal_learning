407ae6c46aea83e73dbc25d0f2d03aaadd4b68f9
A.RCQ <adrien.ricque@cpe.fr>
2025-01-03 22:58:26 +0100

    update cli-app

--- lab_tests/CLIapp/main.go
+++ lab_tests/CLIapp/main.go
@@ -1,12 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	todos := Todos{}
 	todos.add("Buy Milk")
-	todos.add("Buy Avocado")
-	fmt.Printf("%+v\n\n", todos)
-	todos.delete(0)
-	fmt.Printf("%+v\n\n", todos)
+	todos.add("Buy Bread")
+	todos.toggle(0)
+	todos.print()
 }
