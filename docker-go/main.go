92444270648225ce6b929bc67545996fa059da1a
A.RCQ <adrien.ricque@cpe.fr>
2025-06-06 13:35:24 +0200

    new project go-db

--- docker-go/main.go
+++ docker-go/main.go
@@ -3,8 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+<<<<<<< HEAD
 	"io"
 	"net/http"
+=======
+	"log"
+	"os"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+>>>>>>> b88786e (création d'un projet d'interaction go - database)
 )
 
 type Container struct {
@@ -33,3 +44,59 @@ func main() {
 		fmt.Printf("ID: %s, Nom: %s, Image: %s, État: %s\n", container.ID, container.Names, container.Image, container.State)
 	}
 }
+<<<<<<< HEAD
+=======
+
+func ListDockerContainers() ([][3]string, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalf("Unabel to create docker client, please make sure that docker is installed\n%s", err.Error())
+		os.Exit(1)
+	}
+
+	images, err := cli.ImageList(context.Background(), image.ListOptions{})
+	if err != nil {
+		log.Fatal("Unabel to get images, please make sure that docker daemon is up and running")
+		os.Exit(1)
+	}
+
+	var containers [][3]string
+	for _, image := range images {
+		repository := "<none>"
+		tag := "<none>"
+		if len(image.RepoTags) > 0 {
+			splitted := strings.Split(image.RepoTags[0], ":")
+			repository = splitted[0]
+			tag = splitted[1]
+		} else if len(image.RepoDigests) > 0 {
+			repository = strings.Split(image.RepoDigests[0], "@")[0]
+		}
+		containers := append(containers, [3]string{image.ID[7:19], repository, tag})
+		fmt.Println(containers)
+	}
+	return containers, nil
+
+}
+
+func StartContainer(name string) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	containerConfig := &container.Config{
+		Image: name,
+	}
+	container, err := cli.ContainerCreate(context.Background(), containerConfig, nil, nil, "", nil, nil, nil)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if err := cli.ContainerStart(context.Background(), container.ID, types.St); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println("Container started successfully!")
+	return nil
+}
+>>>>>>> b88786e (création d'un projet d'interaction go - database)
