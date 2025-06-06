7b3c1dd0ff755502479ede61f674d90d2fc3a9dc
A.RCQ <adrien.ricque@cpe.fr>
2025-06-06 18:24:31 +0200

    Connection to DB + show an exemple

--- godb_actions/main.go
+++ godb_actions/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -14,13 +17,31 @@ func main() {
 	fmt.Println("Enter your login")
 	fmt.Scanln(&sql_password)
 	fmt.Println("Enter your password")
-	sql_credentials := sql_login + ":" + sql_password + "@tcp(" + sql_address + ")/test"
+	sql_credentials := sql_login + ":" + sql_password + "@tcp(" + sql_address + ")/openbar"
 
 	db, err := sql.Open("mysql", sql_credentials)
 
 	// if there is an error opening the connection, handle it :
 	if err != nil {
 		panic(err.Error())
+	}
+	fmt.Println("Connexion réussie à la base de données MySQL!")
+
+	// Exemple : lecture de données
+	rows, err := db.Query("SELECT * FROM todo")
+	if err != nil {
+		log.Fatalf("Erreur lors de la requête : %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var nom_tache string
+		var date string
+		err := rows.Scan(&nom_tache, &date)
+		if err != nil {
+			log.Fatalf("Erreur de lecture de ligne : %v", err)
+		}
+		fmt.Printf("Name: %s, Date: %s\n", nom_tache, date)
 	}
 
 	// defer the close till after the main function has finished
